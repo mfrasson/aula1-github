04f7b628a4a680ee386d9969c63bc24a64ea2087
Mauricio Frasson Junior <m.frasson.jr@gmail.com>
2018-10-27 16:19:06 -0300

    Novas adicoes de enunciacao

---
+++ src/section8/entities/Comment.java
@@ -0,0 +1,20 @@
+package section8.entities;
+
+public class Comment {
+	
+	private String text;
+	
+	public Comment() { }
+
+	public Comment(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
